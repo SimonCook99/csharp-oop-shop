761b120b630b2b2144d061182f14a987b215fac9
Simone Mauro <simon.mauro@virgilio.it>
2022-06-29 14:29:05 +0200

    Aggiungere i file di progetto.

---
+++ Prodotto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Prodotto{
+        private int codice { get; }
+        string nome { get; set; }
+        string descrizione { get; set;}
+
+        double prezzo { get; set; }
+
+        int ivaPercentuale { get; set; }
+
+        public Prodotto(string nome, string descrizione, double prezzo, int ivaPercentuale)
+        {
+            this.codice = new Random().Next(100, 1001);
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.ivaPercentuale = ivaPercentuale;
+        }
+
+        public double getPrezzo(){
+            return this.prezzo;
+        }
+
+        public double getPrezzoConIVA(){
+            return this.prezzo + (this.prezzo / 100 * this.ivaPercentuale);
+        }
+
+        public string getFullName(){
+            return this.codice.ToString() + " " + this.nome;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+using csharp_oop_shop;
+
+Prodotto p = new Prodotto("T-shirt-casual", "Una T-shirt casual per le situazioni più disparate", 12.50, 22);
+
+Console.WriteLine(p.getPrezzo());
+
+Console.WriteLine(p.getPrezzoConIVA());
+
+Console.WriteLine(p.getFullName());
